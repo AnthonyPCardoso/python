f982828190a80f313e9e64030ee6bbb09803c641
AnthonyPCardoso <anthonydev0708@outlook.com>
2023-08-20 22:19:19 -0300

    Interface

--- simulador_dedado.py
+++ simulador_dedado.py
@@ -1,21 +1,33 @@
 import random
+import PySimpleGUI
+import PySimpleGUI as sg
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de gerar um novo valor? '
-
-    
+        self.mensagem = 'Você gostaria de gerar um novo valor?'
+        # Layout n ta sendo lido
+        layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('Sim'),sg.Button('Não')]
+        ]
+        
     def Iniciar(self):
+        # Create a Windows
+        janela = sg.Window('Simulador de Dado', layout=layout)
+        # Ler os valores da tela
+        self.eventos, self.valores = janela.Read()
         resposta = input(self.mensagem).upper()
-        if resposta == "SIM" or resposta == 'S':
-            self.GerarValorDoDado()
-        elif resposta == "NÃO" or resposta == "N":
-            print("Ok,Obrigado")
-        else:
-            print("Por favor,Digite Sim ou Não")
-
+        try:
+            if resposta == "SIM" or resposta == 'S':
+                self.GerarValorDoDado()
+            elif resposta == "NÃO" or resposta == "N":
+                print("Ok,Obrigado")
+            else:
+                print("Por favor,Digite Sim ou Não")
+        except:
+            print('Ocorreu um erro ao receber sua resposta')
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo,self.valor_maximo))
