8c0def8a412de2c689848ec3fd9a280c2eea484b
AnthonyPCardoso <anthonydev0708@outlook.com>
2023-09-18 09:35:17 -0300

    ..

--- interfaces/m2.py
+++ interfaces/m2.py
@@ -1,22 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 
 
-class MyApp(App):
+class incrementador(BoxLayout):
+    ...
+
+class Test(App):
     def build(self):
-        box = BoxLayout(orientation='vertical')
-        button= Button(text='Botao 1', font_size=20,  on_release=self.incrementar)
-        self.label = Label(text=' 1', font_size=20)
-        box.add_widget(button)
-        box.add_widget(self.label)
-        return box
+        return incrementador()
     
 
     def incrementar(self,button):
         self.label.text = str(int(self.label.text)+1)
 
 if __name__ == "__main__":
-    MyApp().run()
+    Test().run()
